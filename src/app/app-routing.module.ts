08c306cc40eedcd24b582d9b9031d87a213ff624
kareemyasser8 <kareem.yasserr@gmail.com>
2023-05-27 08:43:50 +0200

    fixing the routing between welcome/login/signup and home components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 
 const routes: Routes = [
-  {path: '', component: SignupComponent},
-  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {
+    path: '',
+    component: WelcomeComponent,
+    children: [
+      {path: 'login',component: LoginComponent},
+      {path: 'signup',component: SignupComponent},
+    ]
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  }
 ];
 
 @NgModule({
